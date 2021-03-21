5a3115ac9376d90d29b78047cf7133331dca61fe
Александр Пушкин <aleksandrpuskin@MacBook-Pro-Aleksandr.local>
2021-03-21 20:33:13 +0300

    created something

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="logo512.png" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://i.ibb.co/vJXsNJZ/image-one.jpg" />
+      </div>
+      <div>
+        ava + desc
+        </div>
+      <div>
+        My posts
+          <div>
+          New posts
+          </div>
+        <div>
+          <div>
+            post 1
+            </div>
+          <div>
+            post 2
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
