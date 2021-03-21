334705062c4fd46acb3378d5bbbf6859045266ef
Александр Пушкин <aleksandrpuskin@MacBook-Pro-Aleksandr.local>
2021-03-21 21:56:55 +0300

    another step

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import classes from "./Header.module.css";
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={classes.header}>
             <img src="logo512.png" />
         </header>
     );
